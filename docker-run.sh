98ffa53d0ce3fbecac1f83fe58c3c74cbf6c2ffe
Radoslaw Borowiecki <radoslaw.borowiecki@gmail.com>
2021-04-10 15:15:30 +0200

    Run app via command

---
+++ docker-run.sh
@@ -0,0 +1,1 @@
+docker run --name my-spark-app -v ~/spark-apps:/opt/spark-apps -v ~/spark-data:/opt/spark-data -e ENABLE_INIT_DAEMON=false -e SPARK_APPLICATION_ARGS="--txt-source /opt/spark-data/input.txt --txt-expected /opt/spark-data/expected.txt --target-dir /opt/spark-data/out" --net spark-sets_default --link spark-master:spark-master -d bde/spark-app
